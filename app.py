bdfcc0d9f425cbb4f0fd74a4096dcf617562008e
Silvio <silviolima@gmail.com>
2020-07-01 02:53:36 -0300

    Valores estranhos.

--- app.py
+++ app.py
@@ -106,8 +106,10 @@ def main():
                 st.title("R$ "+status[0:3]+'.'+status[3:])
             elif len (status) == 7:  
                 st.title("R$ "+status[0]+'.'+status[1:4]+'.'+status[4:])
-            else:
+            elif len (status) == 8:
                 st.title("R$ "+status[0:2]+'.'+status[2:5]+'.'+status[5:])
+            elif len (status) == 10:
+                st.title("R$ "+status[0]+'.'+status[1:4]+'.'+status[4:7]+'.'+status[7:10)
 
 
            
