b4e45d9701c8dfe955e93e3fcea79163449ac476
Silvio <silviolima@gmail.com>
2020-07-04 14:55:44 -0300

    Incluido imagem na Home

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def main():
         st.markdown("### Escolha:")
         st.markdown("#### - bairro e caracteristicas")
         st.write(" ")
-        image1 = Image.open("chaves.jpeg")
+        image1 = Image.open("chaves.png")
         st.image(image1,caption="",use_column_width=True)
     
 
