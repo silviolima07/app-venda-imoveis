2ec990916eb18b886c446ef3e4b747b5fe70b560
Silvio <silviolima@gmail.com>
2020-10-22 17:31:37 -0300

    teste

--- app.py
+++ app.py
@@ -17,29 +17,14 @@ def load(model_path):
     return model
 
 
+lista_bairros = ['Moema','Itaim Bibi', 'Vila Mariana']
 
-def main():
-    """ ExtraTreesRegressor - Imoveis """
-    
- 
-    html_page = """
-    <div style="background-color:red;padding=10px">
-        <p style='color:white;text-align:center;font-size:20px;font-weight:bold'>IMÓVEIS</p>
-    </div>
-              """
-    st.markdown(html_page, unsafe_allow_html=True)    
+model_Moema = load(open('Modelo_Bairros/ExtraTreesRegressor-Moema.sav','rb'))
 
-    image = Image.open("vende-se.png")
-    st.sidebar.image(image,caption="",use_column_width=True)
+model_Vila_Mariana = load(open('Modelo_Bairros/ExtraTreesRegressor-Vila_Mariana.sav','rb'))
 
-    lista_bairros = ['Moema','Itaim Bibi', 'Vila Mariana']
+model_Itaim_Bibi = load(open('Modelo_Bairros/RandonForestRegressor-Itaim_Bibi.sav','rb'))
 
-
-    model_Moema = load(open('Modelo_Bairros/ExtraTreesRegressor-Moema.sav','rb'))
-
-    model_Vila_Mariana = load(open('Modelo_Bairros/ExtraTreesRegressor-Vila_Mariana.sav','rb'))
-
-    model_Itaim_Bibi = load(open('Modelo_Bairros/RandonForestRegressor-Itaim_Bibi.sav','rb'))
 
 
 def main():
