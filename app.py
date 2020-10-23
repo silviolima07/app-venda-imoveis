232a401a72059d8b63b38ce1d528dc158c987882
Silvio <silviolima@gmail.com>
2020-10-23 08:04:23 -0300

    Sharing Streamlit

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def main():
  
     html_page = """
     <div style="background-color:blue;padding=10px">
-        <p style='color:white;text-align:center;font-size:25px;font-weight:bold'>Predicting the Sale Price of Apartments by Neighborhood</p>
+        <p style='color:white;text-align:center;font-size:25px;font-family:'Times New Roman'>Predicting the Sale Price of Apartments by Neighborhood</p>
     </div>
               """
     st.markdown(html_page, unsafe_allow_html=True)    
